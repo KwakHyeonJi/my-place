81860422a3f02839818bd6b9b5bb6b6fd92f909a
hjKwak <velmute01@gmail.com>
2023-03-18 17:39:22 +0900

    [config] Absolute path

--- webpack.config.js
+++ webpack.config.js
@@ -59,5 +59,14 @@ module.exports = {
   ],
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
 };
