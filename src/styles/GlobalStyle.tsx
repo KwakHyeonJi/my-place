331b793aa39a48f3d41d1a5882195d4a9ed67417
hjKwak <velmute01@gmail.com>
2023-03-22 12:33:34 +0900

    [feat] Global style

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
