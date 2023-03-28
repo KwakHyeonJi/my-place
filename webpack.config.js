b8a31395fcb12ac0032428e1db8a155e1a5b020c
hjKwak <velmute01@gmail.com>
2023-03-28 17:30:37 +0900

    [config] Add contexts path

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,7 @@ module.exports = {
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
+      '@contexts': path.resolve(__dirname, 'src/contexts'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@interfaces': path.resolve(__dirname, 'src/interfaces'),
       '@pages': path.resolve(__dirname, 'src/pages'),
